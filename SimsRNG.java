631a35f1b6d0a711c426115bf2d768d9503afca8
Leon <leonwste@gmail.com>
2023-06-18 22:21:19 +0200

    removed comments

--- SimsRNG.java
+++ SimsRNG.java
@@ -8,8 +8,8 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.List;
-import java.awt.datatransfer.Clipboard; // add this line
-import java.awt.datatransfer.StringSelection; // add this line
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 
 public class SimsRNG extends JFrame {
     private JTable table;
